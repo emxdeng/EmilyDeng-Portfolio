a80bb65962ae37a72376ec001d0dd3fb239a82b8
Emily Deng <emilyxdeng@gmail.com>
2023-11-27 21:48:19 +1100

    Sussy animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,14 +9,35 @@
 </div>
 
 <style>
+	@keyframes fadeInBackground {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+
 	.app {
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+		position: relative;
+	}
+
+	.app::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
 		background-image: url('/Background.jpg');
 		background-size: auto 100%;
 		background-position: center;
 		background-repeat: no-repeat;
+		opacity: 0;
+		animation: fadeInBackground 2s ease-in-out forwards; /* Adjust the duration and timing function as needed */
 	}
 
 	main {
